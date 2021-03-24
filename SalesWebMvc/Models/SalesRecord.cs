796fd45fd0d59705b5bb0847126c592507e0620f
luannagarla <luagarla@gmail.com>
2021-03-24 13:04:30 -0300

    Uptaded

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
